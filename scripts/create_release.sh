5b8d0d87dc4e8e1a341515f6d6b490e137e8fe27
archos <archos@archoslinux.cz>
2024-06-17 19:15:56 +0200

    Přidán skript pro vytváření archívů projektu a jejich přesun

---
+++ scripts/create_release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Název archivu
+ARCHIVE_NAME="phanpy-cz"
+
+# Vytvoření seznamu souborů k vyloučení
+echo "README.md" > .exclude
+echo "LICENSE" >> .exclude
+echo "create_release.sh" >> .exclude
+
+# Vytvoření .tar.gz archivu
+tar --exclude-from=.exclude -czvf $ARCHIVE_NAME.tar.gz .
+
+# Vytvoření .zip archivu
+zip -r $ARCHIVE_NAME.zip . -x@.exclude
+
+# Přesun archivu do adresáře releases (pokud neexistuje, vytvoří ho)
+mkdir -p ~/stažené
+mv $ARCHIVE_NAME.tar.gz ~/stažené/
+mv $ARCHIVE_NAME.zip ~/stažené/
+
+# Odstranění dočasného souboru .exclude
+rm .exclude
+
+echo "Archivy byly vytvořeny a přesunuty do adresáře ~/stažené:"
+echo "~/stažené/$ARCHIVE_NAME.tar.gz"
+echo "~/stažené/$ARCHIVE_NAME.zip"
