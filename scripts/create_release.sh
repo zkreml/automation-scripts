3c53ccb9ed60db74707b1858cc32c7525495f9fc
archos <archos@archoslinux.cz>
2024-06-17 19:29:53 +0200

    opravena cesta pro stařžení souborů

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -16,8 +16,8 @@ zip -r $ARCHIVE_NAME.zip . -x@.exclude
 
 # Přesun archivu do adresáře releases (pokud neexistuje, vytvoří ho)
 mkdir -p ~/stažené
-mv $ARCHIVE_NAME.tar.gz ~/stažené/
-mv $ARCHIVE_NAME.zip ~/stažené/
+mv $ARCHIVE_NAME.tar.gz ~/Stažené/
+mv $ARCHIVE_NAME.zip ~/Stažené/
 
 # Odstranění dočasného souboru .exclude
 rm .exclude
